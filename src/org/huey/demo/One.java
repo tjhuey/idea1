417254bc3f0a1d43f868f329fc93e2cf9f70cbe1
tjhuey <tjhuey@163.com>
2017-04-25 14:47:02 +0800

    Initial commit

---
+++ src/org/huey/demo/One.java
@@ -0,0 +1,12 @@
+package org.huey.demo;
+
+/**
+ * Created by Administrator on 2017/4/25.
+ */
+public class One {
+
+    public  static  void main(String args[]){
+        System.out.println("hello world");
+    }
+
+}
